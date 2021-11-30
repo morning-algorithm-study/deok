aee3c54a3c9001bd40f2bb655adfbb900f79563d
deokhyun-dv <dev.deokhyun@gmail.com>
2021-11-30 09:56:17 +0900

    20211130_lv1_완주하지못한선수

--- programmers/lv2_최댓값최솟값.js
+++ programmers/lv2_최댓값최솟값.js
@@ -3,10 +3,12 @@ const str = "1 2 3 4";
 function solution(str) {
   let arr = str.split(" ");
   let sortedArr = arr.sort((a, b) => a - b);
-  let min = sortedArr[0];
-  let max = sortedArr[sortedArr.length - 1];
-  let joinText = min.concat(" ", max);
-  return joinText;
+  return sortedArr[0] + " " + sortedArr[sortedArr.length - 1];
+}
+
+function solution2(s) {
+  const arr = s.split(" ");
+  return Math.min(...arr) + " " + Math.max(...arr);
 }
 
 solution(str);
